423da81ad77365ec239503339d03c60ef7923f57
Hack Reactor Students <github@hackreactor.com>
2013-07-01 11:46:23 -0700

    Starts to build basic chat functionality

--- js/setup.js
+++ js/setup.js
@@ -12,3 +12,43 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-Application-Id", "voLazbq9nXuZuos9hsmprUz7JwM2N0asnPnUcI7r");
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
+
+
+$(document).ready(function() {
+  var username = window.location.search.slice(10);
+
+  $(".submit").click(function(){
+    var value = $(".chat")[0].value;
+    console.log(value);
+
+    var message = {};
+    message.username = username;
+    message.text = value;
+
+    $.ajax({
+      type: "POST",
+      url: 'https://api.parse.com/1/classes/messages',
+      data: message,
+      contentType: 'application/json',
+      success: function(){
+        console.log("successful POST");
+      },
+      error: function() {
+        console.log('POST failed');
+      }
+    });
+
+
+    $(".chat")[0].value = "";
+  });
+
+
+
+
+
+
+
+
+
+
+});
